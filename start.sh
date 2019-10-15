901764d054cc20e8fb135e73e344b689e2b18579
IKERRIZ <okothfaith94@gmail.com>
2019-10-15 10:31:33 +0300

    deployment to heroku

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
-export NEWS_API_KEY=eba0e1e21fa146c1821241a91d75e783
-export SECRET_KEY=7995wsc
+export NEWS_API_KEY='eba0e1e21fa146c1821241a91d75e783'
+export SECRET_KEY='7995wsc'
 
 python3.6 manage.py server
